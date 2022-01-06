deca24c77581a81c013d8f9afcb950e9721619cc
Pravinlkr <pravinlakhisarai@gmail.com>
2022-01-06 11:40:24 +0530

    updated

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var serveStatic = require('serve-static');
+const path = require('path')
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(path.join(__dirname , "./dist")));
 var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.listen(port, ()=> 
+console.log('server started '+ port)
+);
