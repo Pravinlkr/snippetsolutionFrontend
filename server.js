921f64d84ee8b9b6b0daffe9bf6d6d803a39874a
Pravinlkr <pravinlakhisarai@gmail.com>
2022-01-06 12:02:05 +0530

    updated

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 app = express();
 app.use(express.static(path.join(__dirname , "./dist")));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, './dist', 'index.html'));
+    res.sendFile(path.join(__dirname, './dist'));
   });
 var port = process.env.PORT || 5000;
 app.listen(port, ()=> 
