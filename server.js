c65d4a6bb3c234283f12704fcb5b7d45a6dc150d
Pravinlkr <pravinlakhisarai@gmail.com>
2022-01-06 11:57:59 +0530

    updated

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 const path = require('path')
 app = express();
 app.use(express.static(path.join(__dirname , "./dist")));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, './dist', 'index.html'));
+  });
 var port = process.env.PORT || 5000;
 app.listen(port, ()=> 
 console.log('server started '+ port)
